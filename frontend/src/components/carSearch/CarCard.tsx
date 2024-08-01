4ff6abaac36dd2e7402dfd51ea3a0dc2ea84f788
jann712 <joao.augustonn1@gmail.com>
2024-08-01 17:37:21 -0300

    search page concept mostly done

--- frontend/src/components/carSearch/CarCard.tsx
+++ frontend/src/components/carSearch/CarCard.tsx
@@ -10,7 +10,7 @@ type CarType = {
 
 export default function CarCard(props: CarType) {
     return (
-        <div className="rounded border-4 border-orange-300 p-5 bg-orange-100 w-auto h-38">
+        <div className="rounded border-4 border-orange-300 p-5 bg-orange-100 w-auto max-h-40">
             <h2 className="text-lg text-center font-bold antialiased">{props.name}</h2>
             <h4 className="text-md font-semibold text-center">R${props.price}</h4>
 
